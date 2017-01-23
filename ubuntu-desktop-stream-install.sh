2a32d3cd6609cadb0b3de9c6ee7e82b56148ea7e
Antony Ho <ntonyworkshop@gmail.com>
2017-01-24 00:15:45 +0800

    Add -y flag to install without interactive

--- ubuntu-desktop-stream-install.sh
+++ ubuntu-desktop-stream-install.sh
@@ -2,6 +2,6 @@
 
 # Ubuntu Desktop Streaming Preparation Script
 
-sudo add-apt-repository ppa:obsproject/obs-studio
+sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo apt-get update
-sudo apt-get install obs-studio
+sudo apt-get install -y obs-studio
